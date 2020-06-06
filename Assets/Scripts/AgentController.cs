7d4afbbf01379bd1bab3c021c7226b7e95a4f603
Supawat Tamsri <meep@supawat.dev>
2020-06-06 23:16:51 +0200

    added controller

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+    float health_point = 100;
+    float hungry_point = 0;
+    float poison = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0f, -.01f, 0f);
+            //Debug.Log("Hello there");)
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, .01f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, 0f, -1f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 0f, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/GenerateOnPlane.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateOnPlane : MonoBehaviour
+{
+    public GameObject selectedPlane;
+    public GameObject[] objectsToGenerate;
+    public int amount = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform rectangle = selectedPlane.transform;
+        
+        for (int i = 0; i < objectsToGenerate.Length; ++i)
+        {
+            for(int j = 0; j < amount; ++ j)
+                Instantiate(objectsToGenerate[i], new Vector3(Random.Range(-15,15),1, Random.Range(-15,15)), Quaternion.Euler(270,0,0) );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
